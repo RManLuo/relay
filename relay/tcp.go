a3aed7e36220dcf5f6f8737b8b461b6c20d00fe1
zcmimi <zcmimi@outlook.com>
2021-05-22 16:28:09 +0800

    .

--- relay/tcp.go
+++ relay/tcp.go
@@ -11,7 +11,7 @@ func (s *Relay) RunTCPServer() error {
 	var err error
 	s.TCPListen, err = net.ListenTCP("tcp", s.TCPAddr)
 	if err != nil {
-		fmt.Print(s.Local, "<=>", s.Remote, err)
+		fmt.Print("Listen", s.Local, err)
 		return err
 	}
 	defer s.TCPListen.Close()
@@ -20,12 +20,14 @@ func (s *Relay) RunTCPServer() error {
 		c, err := s.TCPListen.AcceptTCP()
 		if err != nil {
 			if err, ok := err.(net.Error); ok && err.Temporary() {
+				fmt.Print("Accept", s.Local, err)
 				continue
 			}
 			count++
 			if count > 10 {
 				break
 			}
+			time.Sleep(10 * time.Second)
 			continue
 		}
 		go s.TCPHandle(c)
@@ -38,6 +40,7 @@ func (s *Relay) TCPHandle(c *net.TCPConn) error {
 	defer c.Close()
 	rc, err := net.DialTimeout("tcp", s.Remote, time.Duration(s.TCPTimeout)*time.Second)
 	if err != nil {
+		fmt.Print("Dial", s.Local, "<=>", s.Remote, err)
 		return err
 	}
 	defer rc.Close()
